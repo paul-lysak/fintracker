f825aa241d6ccc9da9b8ecaaeeec88e7cfa184ee
Paul Lysak <paul.lysak@gmail.com>
2012-09-17 16:11:00 +0300

    CSV representation skeleton

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ fintracker
 ==========
 
 Personal finance accounting with frontend in Dojo and data storage in CouchDB
+
+Needs CouchDB version 1.1 or later due to modules usage in design documents.
---
+++ scripts/update_design_doc.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#Needs curl utility to run
+COUCHDB_URI=http://localhost:5984/
+EXPENSES_URI=${COUCHDB_URI}fintracker_expenses/
+EXP_DESIGN_URI=${EXPENSES_URI}_design/logic
+
+old_design=$(curl -s -X GET $EXP_DESIGN_URI)
+
+case "$old_design" in
+	*error* ) 
+	echo "skip deletion of _design/logic"
+	;;
+	*) 
+	rev=$(echo $old_design | sed 's/.\+"_rev":"\([0-9a-z\-]\+\)".\+/\1/');
+	echo removing _design/logic document with rev=$rev...
+	curl -s -X DELETE $EXP_DESIGN_URI?rev=$rev
+	;;
+esac
+echo uploading _design/logic document...
+curl -T../app/js/db/logic.json $EXP_DESIGN_URI
+echo design doc update complete
+
